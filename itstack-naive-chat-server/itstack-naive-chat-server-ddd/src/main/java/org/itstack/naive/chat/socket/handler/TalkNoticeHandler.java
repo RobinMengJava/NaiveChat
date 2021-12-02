f5c1400e0e6aab6e41a63eef17c30bf153c5f9b3
mengweilu <1079501602@qq.com>
2021-12-02 12:06:39 +0800

    对话款提醒功能

--- itstack-naive-chat-server/itstack-naive-chat-server-ddd/src/main/java/org/itstack/naive/chat/socket/handler/TalkNoticeHandler.java
+++ itstack-naive-chat-server/itstack-naive-chat-server-ddd/src/main/java/org/itstack/naive/chat/socket/handler/TalkNoticeHandler.java
@@ -31,36 +31,75 @@ public class TalkNoticeHandler extends MyBizHandler<TalkNoticeRequest> {
 
     @Override
     public void channelRead(Channel channel, TalkNoticeRequest msg) {
-        logger.info("对话通知应答处理：{}", JSON.toJSONString(msg));
+
+        logger.info("================>>>>>> 新增对话框请求参数=[{}]", JSON.toJSONString(msg));
 
         switch (msg.getTalkType()) {
             case 0:
                 // 1. 对话框数据落库
                 userService.addTalkBoxInfo(msg.getUserId(), msg.getFriendUserId(), 0);
                 userService.addTalkBoxInfo(msg.getFriendUserId(), msg.getUserId(), 0);
-                // 2. 查询对话框信息[将自己发给好友的对话框中]
+
+                // 2. 将自己发送到好友的对话框中
                 UserInfo userInfo = userService.queryUserInfo(msg.getUserId());
-                // 3. 发送对话框消息给好友
                 TalkNoticeResponse response = new TalkNoticeResponse();
                 response.setTalkId(userInfo.getUserId());
-                response.setTalkName(userInfo.getUserNickName());
                 response.setTalkHead(userInfo.getUserHead());
-                response.setTalkSketch(null);
+                response.setTalkName(userInfo.getUserNickName());
                 response.setTalkDate(new Date());
-                // 获取好友通信管道
+                response.setTalkSketch(null);
+
+                // 获取好友管道
                 Channel friendChannel = SocketChannelUtil.getChannel(msg.getFriendUserId());
                 if (null == friendChannel) {
-                    logger.info("用户id：{}未登录！", msg.getFriendUserId());
+                    logger.info("=========>>>>> 好友不在线，friendId=[{}]", msg.getFriendUserId());
                     return;
                 }
-                friendChannel.writeAndFlush(response);
+                channel.writeAndFlush(response);
                 break;
+
             case 1:
+                // 群组直接落库对话框
                 userService.addTalkBoxInfo(msg.getUserId(), msg.getFriendUserId(), 1);
                 break;
             default:
                 break;
+
         }
+
+
+
+
+//        logger.info("对话通知应答处理：{}", JSON.toJSONString(msg));
+//
+//        switch (msg.getTalkType()) {
+//            case 0:
+//                // 1. 对话框数据落库
+//                userService.addTalkBoxInfo(msg.getUserId(), msg.getFriendUserId(), 0);
+//                userService.addTalkBoxInfo(msg.getFriendUserId(), msg.getUserId(), 0);
+//                // 2. 查询对话框信息[将自己发给好友的对话框中]
+//                UserInfo userInfo = userService.queryUserInfo(msg.getUserId());
+//                // 3. 发送对话框消息给好友
+//                TalkNoticeResponse response = new TalkNoticeResponse();
+//                response.setTalkId(userInfo.getUserId());
+//                response.setTalkName(userInfo.getUserNickName());
+//                response.setTalkHead(userInfo.getUserHead());
+//                response.setTalkSketch(null);
+//                response.setTalkDate(new Date());
+//                // 获取好友通信管道
+//                Channel friendChannel = SocketChannelUtil.getChannel(msg.getFriendUserId());
+//                if (null == friendChannel) {
+//                    logger.info("用户id：{}未登录！", msg.getFriendUserId());
+//                    return;
+//                }
+//                friendChannel.writeAndFlush(response);
+//                break;
+//            case 1:
+//                userService.addTalkBoxInfo(msg.getUserId(), msg.getFriendUserId(), 1);
+//                break;
+//            default:
+//                break;
+//        }
 
     }
 
