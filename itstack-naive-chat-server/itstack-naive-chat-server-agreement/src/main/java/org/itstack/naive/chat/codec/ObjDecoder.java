5591ab512fd91815f7d7d46d3c2fbcfc79117793
robin <RobinMeng2021@qq.com>
2021-12-02 23:38:30 +0800

    解码和编码协议的深入理解

--- itstack-naive-chat-server/itstack-naive-chat-server-agreement/src/main/java/org/itstack/naive/chat/codec/ObjDecoder.java
+++ itstack-naive-chat-server/itstack-naive-chat-server-agreement/src/main/java/org/itstack/naive/chat/codec/ObjDecoder.java
@@ -9,10 +9,11 @@ import org.itstack.naive.chat.util.SerializationUtil;
 import java.util.List;
 
 /**
- * 解码器
- * 虫洞栈：https://bugstack.cn
- * 公众号：bugstack虫洞栈  ｛关注获取学习源码｝
- * Create by 小傅哥 on 2019
+ * 这是我们自定义的解码器
+ * 如果自定义解码器，需要继承netty提供的解码器抽象类ByteToMessageDecoder，并实现里面的decode方法
+ *
+ * 这里我们实现了自己的解码器，并且自己解决了粘包、半包问题
+ *
  */
 public class ObjDecoder extends ByteToMessageDecoder {
 
@@ -21,15 +22,27 @@ public class ObjDecoder extends ByteToMessageDecoder {
         if (in.readableBytes() < 4) {
             return;
         }
+
+        // 标记包头开始的位置
         in.markReaderIndex();
+        // 读取一个int的长度，也就是要读取存放消息长度的字段的值
         int dataLength = in.readInt();
+        // 如果剩下的可读的字节长度小于消息长度，则重置读指针至原来读取的开始位置，等待消息到达
         if (in.readableBytes() < dataLength) {
             in.resetReaderIndex();
             return;
         }
-        byte command = in.readByte();  //读取指令
+
+        // 如果剩下的可读的字节长度大于等于消息长度，则进行读取指令
+        byte command = in.readByte();  //读取指令、
+
+        // 读取完消息长度和指令后，剩下的就是消息体内容，创建一个存放下消息体长度的字节数据
         byte[] data = new byte[dataLength - 1]; //指令占了一位，剔除掉
+
+        // 将消息体存放进字节数组中
         in.readBytes(data);
+
+        // 将其反序列化为对象
         out.add(SerializationUtil.deserialize(data, Packet.get(command)));
     }
 
--- itstack-naive-chat-server/itstack-naive-chat-server-agreement/src/main/java/org/itstack/naive/chat/codec/ObjEncoder.java
+++ itstack-naive-chat-server/itstack-naive-chat-server-agreement/src/main/java/org/itstack/naive/chat/codec/ObjEncoder.java
@@ -7,17 +7,21 @@ import org.itstack.naive.chat.protocol.Packet;
 import org.itstack.naive.chat.util.SerializationUtil;
 
 /**
- * 编码器
- * 虫洞栈：https://bugstack.cn
- * 公众号：bugstack虫洞栈  ｛关注获取学习源码｝
+ * 自定义解码器
+ * 自定义解码器需要继承MessageToByteEncoder抽象类，并实现里面的encode方法
+ *
  */
 public class ObjEncoder extends MessageToByteEncoder<Packet> {
 
     @Override
     protected void encode(ChannelHandlerContext ctx, Packet in, ByteBuf out) {
+        // 将传入的对象序列化成字节数组
         byte[] data = SerializationUtil.serialize(in);
+        // 写入消息长度
         out.writeInt(data.length + 1);
+        // 添加指令
         out.writeByte(in.getCommand()); //添加指令
+        // 写入字节数据信息
         out.writeBytes(data);
     }
 
