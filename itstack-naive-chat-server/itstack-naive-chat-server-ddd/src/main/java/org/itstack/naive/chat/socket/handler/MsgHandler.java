50f1d51830d7dbcb2ff26434a9687af7651c5ed1
robin <RobinMeng2021@qq.com>
2021-12-03 22:55:48 +0800

    用户与好友通信

--- itstack-naive-chat-server/itstack-naive-chat-server-ddd/src/main/java/org/itstack/naive/chat/socket/handler/MsgHandler.java
+++ itstack-naive-chat-server/itstack-naive-chat-server-ddd/src/main/java/org/itstack/naive/chat/socket/handler/MsgHandler.java
@@ -26,19 +26,37 @@ public class MsgHandler extends MyBizHandler<MsgRequest> {
 
     @Override
     public void channelRead(Channel channel, MsgRequest msg) {
-        logger.info("消息信息处理：{}", JSON.toJSONString(msg));
-        // 异步写库
+        logger.info("==========>>>>> 消息信息处理入参=[{}]", JSON.toJSONString(msg));
+
+        // 1. 异步写库
         userService.asyncAppendChatRecord(new ChatRecordInfo(msg.getUserId(), msg.getFriendId(), msg.getMsgText(), msg.getMsgType(), msg.getMsgDate()));
-        // 添加对话框[如果对方没有你的对话框则添加]
+
+        // 2. 添加对话框
         userService.addTalkBoxInfo(msg.getFriendId(), msg.getUserId(), Constants.TalkType.Friend.getCode());
-        // 获取好友通信管道
+
+        // 3. 判断还有是否在线
         Channel friendChannel = SocketChannelUtil.getChannel(msg.getFriendId());
         if (null == friendChannel) {
-            logger.info("用户id：{}未登录！", msg.getFriendId());
+            logger.info("用户id=[{}]未登录", msg.getFriendId());
             return;
         }
-        // 发送消息
+
+        // 4. 给好友发送消息
         friendChannel.writeAndFlush(new MsgResponse(msg.getUserId(), msg.getMsgText(), msg.getMsgType(), msg.getMsgDate()));
+
+//        logger.info("消息信息处理：{}", JSON.toJSONString(msg));
+//        // 异步写库
+//        userService.asyncAppendChatRecord(new ChatRecordInfo(msg.getUserId(), msg.getFriendId(), msg.getMsgText(), msg.getMsgType(), msg.getMsgDate()));
+//        // 添加对话框[如果对方没有你的对话框则添加]
+//        userService.addTalkBoxInfo(msg.getFriendId(), msg.getUserId(), Constants.TalkType.Friend.getCode());
+//        // 获取好友通信管道
+//        Channel friendChannel = SocketChannelUtil.getChannel(msg.getFriendId());
+//        if (null == friendChannel) {
+//            logger.info("用户id：{}未登录！", msg.getFriendId());
+//            return;
+//        }
+//        // 发送消息
+//        friendChannel.writeAndFlush(new MsgResponse(msg.getUserId(), msg.getMsgText(), msg.getMsgType(), msg.getMsgDate()));
     }
 
 }
