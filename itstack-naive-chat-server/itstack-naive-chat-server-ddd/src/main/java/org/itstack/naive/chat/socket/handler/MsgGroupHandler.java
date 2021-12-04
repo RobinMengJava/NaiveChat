e755c9860043fadb31962206ac11a598e40ff388
robin <RobinMeng2021@qq.com>
2021-12-04 23:19:02 +0800

    用户与群组通信

--- itstack-naive-chat-server/itstack-naive-chat-server-ddd/src/main/java/org/itstack/naive/chat/socket/handler/MsgGroupHandler.java
+++ itstack-naive-chat-server/itstack-naive-chat-server-ddd/src/main/java/org/itstack/naive/chat/socket/handler/MsgGroupHandler.java
@@ -1,5 +1,6 @@
 package org.itstack.naive.chat.socket.handler;
 
+import com.alibaba.fastjson.JSON;
 import io.netty.channel.Channel;
 import io.netty.channel.group.ChannelGroup;
 import org.itstack.naive.chat.application.UserService;
@@ -26,25 +27,51 @@ public class MsgGroupHandler extends MyBizHandler<MsgGroupRequest> {
 
     @Override
     public void channelRead(Channel channel, MsgGroupRequest msg) {
+        logger.info("=============>>>>> 用户与群组通信入参=[{}]", JSON.toJSONString(msg));
+
         // 获取群组通信管道
         ChannelGroup channelGroup = SocketChannelUtil.getChannelGroup(msg.getTalkId());
         if (null == channelGroup) {
             SocketChannelUtil.addChannelGroup(msg.getTalkId(), channel);
             channelGroup = SocketChannelUtil.getChannelGroup(msg.getTalkId());
         }
+
         // 异步写库
         userService.asyncAppendChatRecord(new ChatRecordInfo(msg.getUserId(), msg.getTalkId(), msg.getMsgText(), msg.getMsgType(), msg.getMsgDate(), Constants.TalkType.Group.getCode()));
+
         // 群发消息
+        MsgGroupResponse response = new MsgGroupResponse();
+
         UserInfo userInfo = userService.queryUserInfo(msg.getUserId());
-        MsgGroupResponse msgGroupResponse = new MsgGroupResponse();
-        msgGroupResponse.setTalkId(msg.getTalkId());
-        msgGroupResponse.setUserId(msg.getUserId());
-        msgGroupResponse.setUserNickName(userInfo.getUserNickName());
-        msgGroupResponse.setUserHead(userInfo.getUserHead());
-        msgGroupResponse.setMsg(msg.getMsgText());
-        msgGroupResponse.setMsgType(msg.getMsgType());
-        msgGroupResponse.setMsgDate(msg.getMsgDate());
-        channelGroup.writeAndFlush(msgGroupResponse);
+
+        response.setUserId(userInfo.getUserId());
+        response.setUserHead(userInfo.getUserHead());
+        response.setUserNickName(userInfo.getUserNickName());
+        response.setMsg(msg.getMsgText());
+        response.setMsgType(msg.getMsgType());
+        response.setMsgDate(msg.getMsgDate());
+        response.setTalkId(msg.getTalkId());
+
+        channelGroup.writeAndFlush(response);
+//        // 获取群组通信管道
+//        ChannelGroup channelGroup = SocketChannelUtil.getChannelGroup(msg.getTalkId());
+//        if (null == channelGroup) {
+//            SocketChannelUtil.addChannelGroup(msg.getTalkId(), channel);
+//            channelGroup = SocketChannelUtil.getChannelGroup(msg.getTalkId());
+//        }
+//        // 异步写库
+//        userService.asyncAppendChatRecord(new ChatRecordInfo(msg.getUserId(), msg.getTalkId(), msg.getMsgText(), msg.getMsgType(), msg.getMsgDate(), Constants.TalkType.Group.getCode()));
+//        // 群发消息
+//        UserInfo userInfo = userService.queryUserInfo(msg.getUserId());
+//        MsgGroupResponse msgGroupResponse = new MsgGroupResponse();
+//        msgGroupResponse.setTalkId(msg.getTalkId());
+//        msgGroupResponse.setUserId(msg.getUserId());
+//        msgGroupResponse.setUserNickName(userInfo.getUserNickName());
+//        msgGroupResponse.setUserHead(userInfo.getUserHead());
+//        msgGroupResponse.setMsg(msg.getMsgText());
+//        msgGroupResponse.setMsgType(msg.getMsgType());
+//        msgGroupResponse.setMsgDate(msg.getMsgDate());
+//        channelGroup.writeAndFlush(msgGroupResponse);
     }
 
 }
